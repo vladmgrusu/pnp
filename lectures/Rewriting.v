222f1231a31abb83aa6cdf9d157549f98278c12c
Vlad Rusu <Vlad.Rusu@inria.fr>
2024-01-15 15:41:02 +0100

    did Rewriting.v

--- lectures/Rewriting.v
+++ lectures/Rewriting.v
@@ -55,7 +55,6 @@ predicate.
 Set Implicit Arguments.
 Inductive my_eq (A : Type) (x : A) : A -> Prop :=  my_eq_refl : my_eq x x.
 Notation "x === y" := (my_eq x y) (at level 70).
-
 (** 
 
 As we can see, this definition literally repeats the Coq's standard
@@ -145,8 +144,7 @@ pose D x := if x is 2 then False else True.
 ]]
 *)
 
-have D1: D 1. 
-by [].
+have D1: D 1 by [].
 
 (**
 
@@ -159,7 +157,8 @@ by [].
 ]]
 *)
 
-case: H D1. 
+case: H D1.
+
 
 (**
 
@@ -349,7 +348,7 @@ universally-quantified variables should be instantiated (i.e., with
 
 *)
 
-by rewrite [y + _]addnC.
+by rewrite [y + _] addnC.
 Qed.
 
 Goal forall x y z, (x + (y + z)) = (z + y + x).
@@ -715,12 +714,21 @@ Let us change the statement of a lemma [disaster] for a little bit:
 Lemma disaster2 : 1 === 2 -> False.
 
 (**
-Now, try to prove it using the same scheme. What goes wrong and how to
-fix it?
+my proof
 *)
 Proof.
-(* fill in your proof here instead of [admit] *)
-Admitted.
+move => H; inversion H.
+Restart.
+
+
+(*from solutions*)
+move=> H.
+pose D x := if x is 1 then False else True.
+have D2: D 2. 
+by [].
+by case: H D2 => /=.
+Qed.
+
 
 
 (**
@@ -735,9 +743,16 @@ Lemma rewrite_is_fun T (f : T -> T -> T) (a b c : T):
   commutative f -> associative f ->
   f (f b a) c = f a (f c b).     
 Proof.
-(* fill in your proof here instead of [admit] *)
-Admitted.
+(* my proof *)
+rewrite /commutative  /associative .
+move => Hc Ha.
+by rewrite [f b _] Hc [f c _] Hc -Ha.
+Restart.
 
+(* from solutions*)
+move=> H1 H2.
+by rewrite [(f b a)]H1 -H2 [(f c b)]H1.
+Qed.
 
 
 (**
@@ -747,24 +762,68 @@ Exercise [Properties of maxn]
 Prove the following lemmas about [maxn].
 *)
 
+
 Lemma max_l m n: n <= m -> maxn m n = m.
 Proof.
-(* fill in your proof here instead of [admit] *)
-Admitted.
+(* my proof *)
+rewrite /maxn.
+by case: leqP=>//. 
+Restart.
+
+(* from solutions*)
+rewrite /maxn. 
+by case: (leqP n m)=>//.
+Qed.
+
 
 
 Lemma succ_max_distr_r n m : (maxn n m).+1 = maxn (n.+1) (m.+1).
 Proof.
-(* fill in your proof here instead of [admit] *)
-Admitted.
+(* my proof *)
+rewrite /maxn.
+case: (leqP m n)=>//;
+case: (leqP (m.+1)  (n.+1)) =>//; move=> H H'; f_equal.
+- rewrite ltnS in H.
+  suff : False => //.
+  apply huh' with (n:= n)(m := m); by split.
+- rewrite ltnS in H.
+  suff : False => //.
+  apply huh' with (n:= n)(m := m); by split.
+Restart.
+
+rewrite /maxn.
+case: leqP=>//H; case:leqP=>//.
+- by rewrite -[n.+1]addn1 -[m.+1]addn1 ltn_add2r ltnNge H.
+by rewrite ltnS leqNgt H.
+Qed.
+
+
 
 
 Lemma plus_max_distr_l m n p: maxn (p + n) (p + m) = p + maxn n m.
 Proof.
-(* fill in your proof here instead of [admit] *)
-Admitted.
+(* my proof *)
+rewrite /maxn.
+case: (leqP (p + m)  (p+n)) => // => Hc; congr (_+_).
+- rewrite leq_add2l in Hc.
+  apply leq_gtF in Hc.
+  by rewrite Hc.
+- rewrite ltn_add2l in Hc.
+  by rewrite Hc. 
+Restart.
+
+(* from solutions*)
+rewrite /maxn.
+case: leqP=>// H1; case: leqP=>//.
+- suff: m <= n by rewrite ltnNge=>H; move/negP.
+  by rewrite leq_add2l in H1.
+suff: n < m by move=>H; rewrite leqNgt H. 
+by rewrite ltn_add2l in H1.
+Qed.
 
 
+
+(*Search _ (?x < ?y).*)
 (** 
 
 Hint: it might be useful to employ the lemmas [ltnNge], [leqNgt],
@@ -825,7 +884,13 @@ Prove the following lemma about [minm] and [maxn]:
 
 Lemma addn_min_max m n : minn m n + maxn m n = m + n.
 Proof.
-(* fill in your proof here instead of [admit] *)
+(* my proof *)
+rewrite /minn /maxn.
+by case: natrelP => // => H; rewrite addnC.
+Qed.
+
+
+
 Admitted.
 
 
